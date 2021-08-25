37c7132860223b490069223521ac3b68eceb1fa1
didivassi <diogovelho@gmail.com>
2021-08-25 14:45:50 +0100

    changed js

--- player.js
+++ player.js
@@ -1,14 +1,32 @@
-function toggleLike() {
-    var x = document.getElementById("heart");
-    x.classList.toggle("fa-heart-broken");
-   }
+
+
+function convert(value) {
+  return Math.floor(value / 60) + ":" + (value % 60 ? value % 60 : '00').toFixed(0).padStart(2, '0');
+}
+  document.addEventListener("DOMContentLoaded", function() {
+
+    const player = document.getElementById("myAudio"); 
+    const play = document.getElementById("play"); 
+    const pause = document.getElementById("pause"); 
+    const currentTime = document.getElementById("currentTime");
+    let progressBar = document.getElementById("progressBar");
+
+    currentTime.innerHTML = "0:00";
+    play.addEventListener("click", function() {   player.play(); });
+    pause.addEventListener("click", function() {   player.pause(); });
+
+    player.addEventListener('loadedmetadata',function(){
+      currentTime.innerHTML =convert(player.duration);
+  },false);
+    
+    player.addEventListener('timeupdate',function(){
+      let currentTimeMs = player.currentTime;
+      let percentPlayed = (currentTimeMs / player.duration) * 100;
+      progressBar.style.width = percentPlayed + "%";
+      currentTime.innerHTML = convert(player.duration - currentTimeMs);
+  },false);
+
+
    
-   var x = document.getElementById("myAudio"); 
-   
-   function playAudio() { 
-     x.play(); 
-   } 
-   
-   function pauseAudio() { 
-     x.pause(); 
-   } 
+
+  });
